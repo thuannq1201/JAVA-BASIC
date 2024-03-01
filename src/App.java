73457b3029f3ca11c6572f2ca3ddc6f79ef3b121
thuannq1201 <mscchannel02@gmail.com>
2024-03-01 15:57:45 +0700

    1

---
+++ src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String mess = "My name is Thuan";
+        System.out.println(">>print: " + mess);
+        System.out.println("Hello, World!");
+    }
+}
